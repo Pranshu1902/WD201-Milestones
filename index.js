372440487faa04906f80b342569d7e34d5fbb3e0
Pranshu1902 <pranshu1902@gmail.com>
2022-07-28 20:41:49 +0530

    completed basic L2 sol

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-function hello() {
-  console.log("Welcome to Node.js, by Pranshu");
-}
+const http = require("http");
+const fs = require("fs");
 
-hello();
+let homeContent = "";
+let formContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", function (err, home) {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("survey/form.html", function (err, form) {
+  if (err) {
+    throw err;
+  }
+  formContent = form;
+});
+
+fs.readFile("project.html", function (err, project) {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer(function (request, response) {
+    let url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/form":
+        response.write(formContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
